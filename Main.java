df7003723508d2d1c89d7da8ee95fbc9f905b275
Luong Manh <luongmanh822@gmail.com>
2024-02-20 14:14:50 +0700

    add convert pound to kg

---
+++ Main.java
@@ -0,0 +1,11 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        int poundNum = 4;
+        double poundValue = 0.45359237;
+        double kgValue;
+        kgValue = poundValue * poundNum;
+        System.out.println(poundNum + " pounds is " + kgValue + " Kg.");
+    }
+}
---
+++ convertPoundToKg.java
@@ -0,0 +1,9 @@
+public class convertPoundToKg {
+        public static void main(String[] args) {
+            int poundNum = 4;
+            double poundValue = 0.45359237;
+            double kgValue;
+            kgValue = poundValue * poundNum;
+            System.out.println(poundNum + " pounds is " + kgValue + " Kg.");
+        }
+}
